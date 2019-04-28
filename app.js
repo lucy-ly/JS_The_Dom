6a04b4fd725e1d33c296f9a85fff78ddc3bb852a
Lucy Ly <lucyly.student@careerdevs.com>
2019-04-28 18:57:22 +0000

    added lesson-15

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-const listItems = document.querySelectorAll('#book-list ul li');
+const list = document.querySelector('#book-list ul');
+const forms = document.forms;
 
-Array.from(listItems).forEach(function(item) {
-    item.addEventListener('click', (e) => {
-
+// delete books
+list.addEventListener('click', (e) => {
+    if (e.target.className == 'delete') {
         const li = e.target.parentElement;
-        console.log('child element to remove:', li);
-        console.log('parent element to remove child from:', li.parentElement);
         li.parentNode.removeChild(li);
-
-    });
+    }
 });
 
-// prevent default behaviour
-
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e) {
+// add books
+const addForm = forms['add-book'];
+addForm.addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log('Navigation to', e.target.textContent, 'was prevented');
+
+    // create elements
+    const value = addForm.querySelector('input[type="text"]').value;
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    // add text content
+    bookName.textContent = value;
+    deleteBtn.textContent = 'delete';
+
+    // add classes
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+    // append to DOM
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+});
+
+// hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e) {
+    if (hideBox.checked) {
+        list.style.display = "none";
+    }
+    else {
+        list.style.display = "initial";
+    }
 });
